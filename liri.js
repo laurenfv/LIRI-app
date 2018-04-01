71f668925f5287bc05b788d87927e97f9f5b0c1e
laurenfv <laurenfventura@gmail.com>
2018-03-31 23:57:06 -0400

    OMDB request is working

--- liri.js
+++ liri.js
@@ -12,7 +12,7 @@ var argv2 = process.argv[2];
 
 var commandTwitter = `my-tweets`;
 var commandSpotify = `spotify-this-song`;
-var commandOmdb = `spotify-this-song`;
+var commandOmdb = `movie-this`;
 var commandFs = `do-what-it-says`;
 
 if (argv2 === commandTwitter){
@@ -47,17 +47,30 @@ else if (argv2 === commandSpotify){
     console.log(data.tracks.items[0].external_urls.spotify);
     });
 }
-// else if (argv2 === commandOmdb){
-//     // //OMDB `movie-this`
-//     // request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
-//     //     // If the request is successful (i.e. if the response status code is 200)
-//     //     if (!error && response.statusCode === 200) {
-//     //         // Parse the body of the site and recover just the imdbRating
-//     //         // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-//     //         console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
-//     //     }
-//     // });
-// }
+else if (argv2 === commandOmdb){
+    var query = process.argv[3];
+
+    //OMDB `movie-this`
+    if (query === undefined){
+        query = "Mr. Nobody"
+    }
+    
+    request("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error) {
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year Released: " + JSON.parse(body).Year);
+            console.log("IMDB rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes rating: " + JSON.parse(body).Ratings[1].value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
+}
 // else if (argv2 === commandFs){
 // //FS RANDOM.TXT `do-what-it-says`
 //     //TO DO: read from random.txt
